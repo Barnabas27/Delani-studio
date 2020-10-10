033fe5105e9de96f300cc4ff2aeee9244df34e43
Barnabas27 <bnyambura032@gmail.com>
2020-10-10 15:58:32 +0300

    add the .toggle() function to the what-we-do section

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $(".toggle").click(function() {
+        $(".design").toggle();
+        $(".par1").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".toggle1").click(function() {
+        $(".development").toggle();
+        $(".par2").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".toggle2").click(function() {
+        $(".project").toggle();
+        $(".par3").toggle();
+    });
+});
